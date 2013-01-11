1cee2964d5cfbfe74ab4fccd32cc426b759695c3
hayashi-m <hayashi.masanori@adniss.jp>
2013-01-11 16:46:06 +0900

    redmineの初期データ(seed)で、作業中の不要なファイルが残っていたので削除

--- plugins/lgdis/db/seeds/users.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-# 削除（idが1より大きい 且つ 100以下 ※1はadminユーザ）
-ActiveRecord::Base.connection.execute(%{DELETE FROM users WHERE id> 1 and id<= 100})
-
-# シーケンス初期化（100以下の場合のみ）
-ActiveRecord::Base.connection.execute(%{SELECT setval( 'users_id_seq' ,CASE WHEN (SELECT MAX(id) FROM users) >= 100 THEN (SELECT MAX(id) FROM users) ELSE 100 END)})
-
-Group.create(id: "2", name: "サンプル2")
