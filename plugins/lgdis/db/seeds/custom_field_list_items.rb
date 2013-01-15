4a1147b335b3e05a2b61065ca68c8f3e1e754ce7
hayashi-m <hayashi.masanori@adniss.jp>
2013-01-15 18:34:58 +0900

    カスタムフィールド設定をコミット

--- plugins/lgdis/db/seeds/custom_field_list_items.rb
+++ plugins/lgdis/db/seeds/custom_field_list_items.rb
@@ -2,7 +2,7 @@
 settings = YAML.load_file(File.join(Rails.root,"plugins","lgdis","config","custom_field_list_items.yml"))
 settings["load_settings"].each {|custom_field_id, file_name|
   item_values = []
-  File.foreach(File.join(settings["load_dir"], file_name)) {|line| item_values << line.chomp}
+  File.foreach(File.join(settings["load_dir"], file_name)) {|line| item_values << line.chomp.gsub(/,/, ":")}
   if item_values.present? && icf = IssueCustomField.find_by_id(custom_field_id)
     #p "#{custom_field_id}:#{icf.name} <<< #{file_name}"
     # カスタムフィールドリスト項目の選択肢の設定
