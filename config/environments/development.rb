2987ffba6d5d51c970395a1e0b8069e9239348cb
Masaru Sato <marphy.986@gmail.com>
2013-01-18 00:11:05 +0900

    small fix.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,5 +15,6 @@ RedmineApp::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
   config.active_support.deprecation = :log
 end
