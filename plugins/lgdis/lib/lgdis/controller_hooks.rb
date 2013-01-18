7038cd3f3c92f5fac332bd96f8c2f0e17fb5d37b
hayashi-m <hayashi.masanori@adniss.jp>
2013-01-18 22:55:52 +0900

    プロジェクト自動作成処理を仮実装

--- plugins/lgdis/lib/lgdis/controller_hooks.rb
+++ plugins/lgdis/lib/lgdis/controller_hooks.rb
@@ -7,16 +7,22 @@ module Lgdis
                          4 => ::FacebookRequestJob
                        }.freeze
 
+    # controller_issues_new_after_saveホック処理
+    # ==== Args
+    # _context_ :: コンテキスト
+    # ==== Return
+    # ==== Raise
     def controller_issues_new_after_save(context={})
       # TODO
       # parser 部に依頼
-      # auto_flag = context[:params][:automatic_delivery_flag]
+      create_project(context) if context[:params][:automatic_create_project_flag]
+      auto_flag = context[:params][:automatic_delivery_flag]
       # test_flag = context[:params][:com_test_flag]
       # destination_ids = context[:params][:issue][:destination_id]
 
       # TODO
       # 仮実装
-      auto_flag = true
+      #auto_flag = true
       test_flag = true
       destination_ids = [3,4]
 
@@ -31,6 +37,29 @@ module Lgdis
           end
         end
       end
+    end
+    
+    private
+    
+    # プロジェクト自動作成処理
+    # ==== Args
+    # _context_ :: コンテキスト
+    # ==== Return
+    # ==== Raise
+    def create_project(context={})
+      # プロジェクトを作成
+      issue = context[:issue]
+      created_on_str = issue.created_on.strftime("%Y%m%d%H%M%S")
+      # TODO:作成情報が未決
+      new_project = Project.create!(name: "#{created_on_str} #{issue.subject}",
+          identifier: "prj-#{created_on_str}",
+          description: "#{issue.description}",
+          #tracker_ids: {},
+          #homepage: "",
+          )
+      
+      # 作成したプロジェクトにチケットをコピー
+      issue.move_to_project(new_project, nil, copy: true)
     end
   end
 end
