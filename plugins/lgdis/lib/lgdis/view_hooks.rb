9834c58b6550708a32f80bc85c6a05fbff5ebd6e
hayashi-m <hayashi.masanori@adniss.jp>
2013-01-18 12:31:08 +0900

    hook名と異なっていたため、ファイル名を変更 _view_issues_form_details_bottom.html.erb⇒_view_issues_show_details_bottom.html.erb

--- plugins/lgdis/lib/lgdis/view_hooks.rb
+++ plugins/lgdis/lib/lgdis/view_hooks.rb
@@ -2,7 +2,7 @@
 module Lgdis
   class ViewHooks < Redmine::Hook::ViewListener
     render_on :view_issues_show_details_bottom,
-              :partial => 'issues/view_issues_form_details_bottom'
+              :partial => 'issues/view_issues_show_details_bottom'
     render_on :view_issues_show_description_bottom,
               :partial => 'issues/view_issues_show_description_bottom'
   end
