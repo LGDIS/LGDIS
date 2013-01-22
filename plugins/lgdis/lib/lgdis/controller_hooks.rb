c91b319213d0470069c92958387b9e1628a79cda
DeveloperMamoruSato <mamoru@carrot.co.jp>
2013-01-22 20:32:43 +0900

    配信管理 自動配信処理修正

--- plugins/lgdis/lib/lgdis/controller_hooks.rb
+++ plugins/lgdis/lib/lgdis/controller_hooks.rb
@@ -11,33 +11,13 @@ module Lgdis
       # TODO
       # parser 部に依頼
       create_project(context) if context[:params][:automatic_create_project_flag]
-      auto_flag = context[:params][:automatic_delivery_flag]
-      # test_flag = context[:params][:com_test_flag]
-      # destination_ids = context[:params][:issue][:destination_id]
 
-      # TODO
-      # 仮実装
-      #auto_flag = true
-      test_flag = false
-      destination_ids = [2]
-      content_delivery =
-        {'mailing_list_name' => DST_LIST['mailing_list']['local_government_officer_mail'], 'title' => context[:params][:issue][:subject], 'message' => context[:params][:issue][:description]}
-
-      if auto_flag
-        # TODO
-        # 配信先に紐付く、配信内容が決まっていない
-        # issue のどの項目(連結して?)を配信するか
-        # redis が起動されている必要がある
-        unless destination_ids.blank?
-          destination_ids.each do |id|
-            Resque.enqueue(DST_LIST['delivery_job_map'][id], content_delivery, test_flag)
-          end
-        end
-      end
+#      deliver_issue(context) if context[:params][:automatic_delivery_flag]
+      deliver_issue(context) if true
     end
-    
+
     private
-    
+
     # プロジェクト自動作成処理
     # ==== Args
     # _context_ :: コンテキスト
@@ -54,9 +34,96 @@ module Lgdis
           #tracker_ids: {},
           #homepage: "",
           )
-      
+
       # 作成したプロジェクトにチケットをコピー
       issue.move_to_project(new_project, nil, copy: true)
+    end
+
+    # 自動配信処理
+    # ==== Args
+    # _context_ :: コンテキスト
+    # ==== Return
+    # ==== Raise
+    def deliver_issue(context)
+      # TODO
+      # 仮実装
+      # test_flag = context[:params][:com_test_flag]
+      # disaster_training_flag = context[:params][:disaster_training_flag]
+      # destination_ids = context[:params][:issue][:destination_id]
+      test_flag = false
+      disaster_training_flag = true
+      destination_ids = [2]
+      issue = context[:params][:issue]
+
+      # TODO
+      # 配信先に紐付く、配信内容未決
+      # TODO
+      # redis が起動されている必要がある
+      unless destination_ids.blank?
+        destination_ids.each do |id|
+          str = DST_LIST['create_msg_msd'][id] + \
+                "(" + "#{issue}" + "," +  "#{disaster_training_flag}" + ")"
+          content_delivery = eval(str)
+
+          Resque.enqueue(eval(DST_LIST['delivery_job_map'][id]),
+                         content_delivery,
+                         test_flag)
+        end
+      end
+    end
+
+    # 公共情報コモンズ用 配信メッセージ作成処理
+    # ==== Args
+    # _issue_ :: チケット情報
+    # _disaster_training_flag_ :: 災害訓練フラグ
+    # ==== Return
+    # ==== Raise
+    def create_commons_msg(issue, disaster_training_flag)
+      # TODO
+      # 配信内容未決
+    end
+
+    # 自治体職員用 配信メッセージ作成処理
+    # ==== Args
+    # _issue_ :: チケット情報
+    # _disaster_training_flag_ :: 災害訓練フラグ
+    # ==== Return
+    # チケットのデータを成形した、メーリングリスト、標題、本文 のHash データ
+    # ==== Raise
+    def create_smtp_msg(issue, disaster_training_flag)
+      # TODO
+      # 配信内容未決
+      content_delivery =
+        {'mailing_list_name' => DST_LIST['mailing_list']['local_government_officer_mail'],
+         'title'   => issue['subject'],
+         'message' => create_disaster_training_str(issue, disaster_training_flag)}
+    end
+
+    # SNS(Twitter, Facebook)用 配信メッセージ作成処理
+    # ==== Args
+    # _issue_ :: チケット情報
+    # _disaster_training_flag_ :: 災害訓練フラグ
+    # ==== Return
+    # チケットの説明 部分の文字列
+    # ==== Raise
+    def create_sns_msg(issue, disaster_training_flag)
+      # TODO
+      # 配信内容未決
+      create_disaster_training_str(issue, disaster_training_flag)
+    end
+
+    # 共通配信メッセージ作成処理
+    # ==== Args
+    # _issue_ :: チケット情報
+    # _disaster_training_flag_ :: 災害訓練フラグ
+    # ==== Return
+    # チケットの説明 部分の文字列（災害訓練付加）
+    # ==== Raise
+    def create_disaster_training_str(issue, flg)
+      # TODO
+      # 配信内容未決
+      str = flg.blank? ? '' : '【災害訓練】'
+      str + issue['description']
     end
   end
 end
