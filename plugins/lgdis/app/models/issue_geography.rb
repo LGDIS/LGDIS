55a6b8ceab8d57653dded97339604be0d11fab8b
hayashi-m <hayashi.masanori@adniss.jp>
2013-02-01 18:28:12 +0900

    issue_geographiesテーブルの修正、RESTチケット登録時のissue_geographies登録処理追加、チケット削除時のissue_geographies削除処理追加

--- plugins/lgdis/app/models/issue_geography.rb
+++ plugins/lgdis/app/models/issue_geography.rb
@@ -1,7 +1,14 @@
 class IssueGeography < ActiveRecord::Base
   unloadable
-
+  
   belongs_to :issue
-  attr_accessible :id,:issue_id,:datum,:location,:point,:line,:polygon,:remarks,
-                  :as => :issue
+  
+  attr_accessible :datum,:location,:point,:line,:polygon,:remarks
+  
+  validates :datum,
+                :length => {:maximum => 10}
+  validates :location,
+                :length => {:maximum => 100}
+  validates :remarks,
+                :length => {:maximum => 255}
 end
