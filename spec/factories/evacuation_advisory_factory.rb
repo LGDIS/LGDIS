d8a060729bcab84277039395240f347cfdc660fd
I.Kobuchi <i.kobuchi@cybermissions.co.jp>
2013-03-15 10:10:48 +0900

    避難勧告モデルのファクトリーファイル修正 spec作成時のコミット漏れ

--- spec/factories/evacuation_advisory_factory.rb
+++ spec/factories/evacuation_advisory_factory.rb
@@ -9,8 +9,8 @@ FactoryGirl.define do
 #     issued_at                       
 #     changed_at                      
 #     lifted_at                       
-    households                      
-    head_count                      
+    households                      10
+    head_count                      11
     identifier                      '04202E00000000000006'
 #     category                        
 #     cause                           
