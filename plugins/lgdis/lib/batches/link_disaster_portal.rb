8c22e0fcb0b81ae4c57eb9316e255079f3bd8c80
I.Kobuchi <i.kobuchi@cybermissions.co.jp>
2013-03-28 16:12:40 +0900

    災害情報ポータル連携、改めRSS情報、抽出条件等、詳細が決まったものを実装

--- plugins/lgdis/lib/batches/link_disaster_portal.rb
+++ plugins/lgdis/lib/batches/link_disaster_portal.rb
@@ -5,18 +5,15 @@
 # 実行環境の指定 :: -e production
 
 require 'cgi'
+ATOM = 9 # RSS配信(Atom)のdelivery_place_id
 
 class Batches::LinkDisasterPortal
   def self.execute
     Rails.logger.info(" #{Time.now.to_s} ===== #{self.name} START ===== ")
-    
-    # プロジェクトIDが未設定、または設定してあるプロジェクトIDのデータが存在しない場合は終了
-    if DST_LIST["link_disaster_portal_project_id"].blank? || Project.where({:id => DST_LIST["link_disaster_portal_project_id"]}).blank?
-      Rails.logger.info(" #{Time.now.to_s} ===== #{self.name} PROJECT ID is not setting. ===== ")
-      Rails.logger.info(" #{Time.now.to_s} ===== #{self.name}  END  ===== ")
-      return
-    end
-    pj = Project.where({:id => DST_LIST["link_disaster_portal_project_id"]}).first
+    time = Time.now # 時間取得
+    limit_days = DST_LIST["link_disaster_portal_limit_days"] # 取得範囲発表からXX日以内
+    max_num    = DST_LIST["link_disaster_portal_max_num"]    # １ファイルあたりの最大チケット件数
+    reject_project_ids =DST_LIST["link_disaster_portal_reject_project_ids"] # 対象外プロジェクトID
     
     auther_name  = DST_LIST["link_disaster_portal_auther_name"]
     auther_email = DST_LIST["link_disaster_portal_auther_email"]
@@ -25,88 +22,96 @@ class Batches::LinkDisasterPortal
     tracker_ids = DST_LIST["link_disaster_portal_tracker_ids"]
     
     tracker_ids.each do | tracker_id |
-      next unless pj.tracker_ids.include?( tracker_id ) # プロジェクトにひも付かないトラッカーはスキップ
       next if DST_LIST["link_disaster_portal_tracker_group"][tracker_id].blank? # 出力項目定義が取得できない場合はスキップ
       
       # 対象トラッカーのチケット取得
-      issues = Issue.where({:tracker_id => tracker_id})
+      issues = Issue.where({:tracker_id => tracker_id}).where("project_id not in (?)", reject_project_ids).order("updated_on DESC") # TODO 対象外プロジェクトの条件を追加する
+      
+      # 出力対象の条件にあてはまらないチケットを削除
+      issues.delete_if do |issue|
+        
+        # 配信管理を取得 配信先がATOM、公開開始日時が現在日時より過去XX日以内(XXは設定ファイルに記述)の中で最新のもの
+        dh = issue.delivery_histories.where(:delivery_place_id => ATOM).order("updated_at DESC").find(:all, :conditions => ["opened_at > CURRENT_TIMESTAMP - interval '#{limit_days} days'"]).first
+        next true if dh.blank? # 配信管理が無かった場合、配信対象なし
+        
+        #以下の２つの条件以外の場合
+        unless ( 
+                 (dh.status == "done"    && (dh.closed_at.blank? ? true : time < dh.closed_at)) || # ステータス配信完了、かつ公開終了日時に至っていない
+                 (dh.status == "runtime" && time > dh.opened_at)                                   # ステータス配信予定、かつ公開開始日時に至っている
+                 # TODO ↑"runtime"は配信中だが、配信予定のステータスが追加される予定
+               )
+          next true
+        end
+        false
+      end
       
       next if issues.blank? # 出力対象のチケットが存在しない場合はスキップ
       
+      # 最大件数以内に絞り込み
+      issues = issues[0..max_num-1]
       # テンプレートの読み込み
       file = File.new("#{Rails.root}/plugins/lgdis/files/xml/atom_with_georss.xml")
       # Xmlドキュメントの生成
       doc  = REXML::Document.new(file)
       
       feed = doc.elements["feed"]
-      feed.elements["title"].text = pj.name
-      
-      #CGI off-line mode回避用ダミーコード:最後にコントロールDを入れる作業を回避
-      ARGV.replace(%w(abc=001 def=002))
-      cgi = CGI.new
-      feed.elements["link"].attributes["href"] += "#{cgi.server_name}/r/feed/"
-      
-      time = Time.now
-      feed.elements["updated"].text = time.xmlschema
+      feed.elements["title"].text = DST_LIST["link_disaster_portal_tracker_constants"][tracker_id]["title"]      
+      feed.elements["updated"].text = time.xmlschema # YYYY-MM-DDThh:mm:ss+9:00 のフォーマットでなくてはならない
       
       author = feed.elements["author"]
-      author.elements["name"].text = auther_name
-      author.elements["email"].text = auther_email
+      author.elements["name"].text =  DST_LIST["link_disaster_portal_tracker_constants"][tracker_id]["name"]
+      author.elements["email"].text = DST_LIST["link_disaster_portal_tracker_constants"][tracker_id]["email"]
       
-      feed.elements["id"].text = "urn:uuid:" + UUIDTools::UUID.random_create.to_s # TODO 後述のUUIDと同じものを使うかどうか確認
+      feed.elements["id"].text = "#{tracker_id}-#{time.strftime("%Y%m%d%H%M%S")}" # TODO 暫定でトラッカーID-YYYYMMDDHH24MISS 
       
       
       # チケットごとのループ
       issues.each do | issue |
+        # 配信管理取得
+        dh = issue.delivery_histories.where(:delivery_place_id => ATOM).order("updated_at DESC").find(:all, :conditions => ["opened_at > CURRENT_TIMESTAMP - interval '#{limit_days} days'"]).first
+        
+        # 配信管理更新、チケット履歴出力
+        if dh.status == "runtime" # TODO 今後「配信中(runtime)」が「配信予定」になるので要修正
+          dh.status = "done"
+          dh.process_date = Time.now
+          dh.save!
+          issue.register_issue_journal_rss_deliver(dh)
+        end
+        
         # XML main entry
         new_entry = REXML::Element.new("entry")
+        training_header = DST_LIST["training_prj"][issue.project_id] ? "【訓練】" : ""
+        new_entry.add_element("title").add_text(training_header + "#{dh.mail_subject}")
+        new_entry.add_element("id").add_text("#{issue.id}-#{time.strftime("%Y%m%d%H%M%S")}") # TODO 暫定でチケットID-YYYYMMDDHH24MISS
+        new_entry.add_element("published").add_text(dh.opened_at.xmlschema)
+        new_entry.add_element("updated").add_text(dh.opened_at.xmlschema)
         
-        new_entry.add_element("title").add_text("#{issue.tracker.name} ##{issue.id}: #{issue.subject}")
-        # TODO サンプルにはここに<link>タグがあるが、出力内容が未定。
-        new_entry.add_element("id").add_text("urn:uuid:" + UUIDTools::UUID.random_create.to_s) # TODO 前述のUUIDと同じものを使うかどうか確認
-        new_entry.add_element("updated").add_text(time.xmlschema)
-        new_entry.add_element("summary").add_text(issue.description.to_s[0,140])
-        
-        content = "<hr><ul>"
+        content = ""
         DST_LIST["link_disaster_portal_tracker_group"][tracker_id].each do | label_value |
           # content 内容作成
-          content += '<li type= "circle "><strong>' + label_value["label"] + ':</strong></li>'
-          content += eval(label_value["value"]) + '<br>'
+          #content += '<p>' + label_value["label"] + ':' + eval(label_value["value"]) + '</p><br />\n' 半角スペースは&nbsp;に変換されないので直接文字参照で記述し「&」をデコードする、 
+          content += '&lt;p&gt;' + label_value["label"] + ':' + eval(label_value["value"]) + '&lt;/p&gt;&lt;br&nbsp;/&gt;\n'
         end
-        content += "</ul>"
         
-        ele_contents = new_entry.add_element("content")
-        ele_contents.add_attribute("type","html")
-        ele_contents.add_text(content)
+        ele_content = new_entry.add_element("content")
+        ele_content.add_attribute("type","html")
+        ele_content.add_text(content)
         
         # XML geo
-        cnt = 0
         points_for_map(issue).each do |point|
-          cnt += 1
-          REXML::Comment.new("-------- 本件についての地理情報 No." + cnt.to_s + " --------", new_entry)
           new_entry.add_element("georss:point").text = point["points"].join(" ")
-          new_entry.add_element("georss:relationshipTag").text = "iconfile=#{rand(16)}-dot.png" # TODO この情報は必要か確認
         end
 
         lines_for_map(issue).each do |line|
-          cnt += 1
-          REXML::Comment.new("-------- 本件についての地理情報 No." + cnt.to_s + " --------", new_entry)
           new_entry.add_element("georss:line").text = line["points"].flatten.join(" ")
-          new_entry.add_element("georss:relationshipTag").text = "iconfile=#{rand(16)}-dot.png" # TODO この情報は必要か確認
         end
 
         polygons_for_map(issue).each do |polygon|
-          cnt += 1
-          REXML::Comment.new("-------- 本件についての地理情報 No." + cnt.to_s + " --------", new_entry)
           new_entry.add_element("georss:polygon").text = polygon["points"].flatten.join(" ")
-          new_entry.add_element("georss:relationshipTag").text = "iconfile=#{rand(16)}-dot.png" # TODO この情報は必要か確認
         end
 
         locations_for_map(issue).each do |location|
-          cnt += 1
-          REXML::Comment.new("-------- 本件についての地理情報 No." + cnt.to_s + " --------", new_entry)
           new_entry.add_element("georss:featureTypeTag").text = location["location"]
-          new_entry.add_element("georss:relationshipTag").text = "iconfile=#{rand(16)}-dot.png" # TODO この情報は必要か確認
         end
         
         feed.add_text(new_entry)
@@ -118,7 +123,7 @@ class Batches::LinkDisasterPortal
       output_file_name = "Tracker#{tracker_id}-#{Time.now.strftime("%Y%m%d_%H%M%S")}-geoatom.xml" # TODO: ファイル名未決定
       
       FileUtils::mkdir_p(output_dir_path) unless File.exist?(output_dir_path) # 出力先ディレクトリを作成
-      File.binwrite(output_dir_path.join(output_file_name.force_encoding("UTF-8")), CGI::unescapeHTML(doc.to_s))
+      File.binwrite(output_dir_path.join(output_file_name.force_encoding("UTF-8")), CGI::unescapeHTML(doc.to_s)) # &amp;→& の為unescapeする
       
     end
     
