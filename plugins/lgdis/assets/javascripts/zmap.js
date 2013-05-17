ddfdc1c4f2ca2809e9764bbe82f751d955810cb9
nkimura <naohirokimura@di-system.co.jp>
2013-05-17 20:51:52 +0900

    宅地図のマーカの中心表示

--- plugins/lgdis/assets/javascripts/zmap.js
+++ plugins/lgdis/assets/javascripts/zmap.js
@@ -1,3 +1,8 @@
+// znet maps api
+// 宅地図のズーム量を調整する定数
+var DEFAULT_ZMAP_ZOOM_UPPER_LIMIT = 9;
+var DEFAULT_ZMAP_ZOOM_LOWER_LIMIT = 4;
+var DEFAULT_ZMAP_ZOOM_CONTROL = 1;
 /** 
  * マップ表示
  * ※ブラウザ上に認証承認ID(ADI)cookie群が存在する前提
@@ -15,8 +20,8 @@ function initZMapSelectPosition(map_id, zoom_rate, field_id, url) {
   }
 
   // マップ作成用パラメータ指定
-  var w = '600';
-  var h = '480';
+  var w = '470';
+  var h = '280';
   var opts = new ZntMapOptions();
 
   // 緯度経度を指定する
@@ -63,9 +68,14 @@ function initZMapSelectPosition(map_id, zoom_rate, field_id, url) {
   if (text_field_value.length > 0) {
     var marker_bounds = null;
     var addresses = text_field_value.split("\n");
+    var addresses_total = 0;
+    addresses_total = addresses.length;
     for (var i = 0; i < addresses.length; i++) {
+      if (i == 0){
+        rect_Reset();
+      }
       // 入力された住所にマーカをセットする
-      addZMarker(addresses[i], target, marker_bounds,url);
+      addZMarker(addresses[i], target, marker_bounds, url, addresses_total);
     }
   }
 
@@ -75,16 +85,21 @@ function initZMapSelectPosition(map_id, zoom_rate, field_id, url) {
   });
 }
 
+// 矩形範囲の変数
+var x_max = 0;
+var x_min = 0;
+var y_max = 0;
+var y_min = 0;
+var ztmp_zoom = 0;
 /**
  * マップにマーカを表示(発生場所入力用)
  */
-function addZMarker(address, target, marker_bounds, url) {
+function addZMarker(address, target, marker_bounds, url, addresses_total) {
   if (!address) return;
 
   // 検索オブジェクトと検索条件指定オブジェクトを生成する
   var sear = new ZntAddressSearch();
   var opts = new ZntAddressSearchSettings();
-
   // 検索ワードを指定する
   opts.freeWord = address;
   // 都道府県コードを指定する
@@ -103,13 +118,51 @@ function addZMarker(address, target, marker_bounds, url) {
   sear.addEventListener("receive", function(r){
     var result = r.result;
     if (result.status == "30200000") {
-      if (result.itemsAddr.length > 0) {
-        // 取得地点にマーカをセット
+      if (addresses_total > 1) {
+      	// 取得地点にマーカをセット
         putMarker(target, result.itemsAddr[0].pos, url);
         // マップ表示をマーカ群を見渡せる範囲に拡張する
-        marker_bounds = extendRect(marker_bounds, result.itemsAddr[0].pos);
+        // 矩形の変数の値が「0]なら初期値として一つめの発生場所の座標を設定する
+        if (x_max == 0){
+          x_max = result.itemsAddr[0].pos.x;
+          x_min = result.itemsAddr[0].pos.x;
+          y_max = result.itemsAddr[0].pos.y;
+          y_min = result.itemsAddr[0].pos.y;
+        } else {
+          if (x_max < result.itemsAddr[0].pos.x){
+            x_max = result.itemsAddr[0].pos.x;
+          }
+          if (x_min > result.itemsAddr[0].pos.x){
+            x_min = result.itemsAddr[0].pos.x;
+          }
+          if (y_max < result.itemsAddr[0].pos.y){
+            y_max = result.itemsAddr[0].pos.y;
+          }
+          if (y_min > result.itemsAddr[0].pos.y){
+            y_min = result.itemsAddr[0].pos.y;
+          }
+        }
+        marker_bounds = new ZntRect(new ZntPoint(x_min, y_min), new ZntPoint(x_max, y_max));
         target.map.moveToRect(marker_bounds, new ZntSize(20, 20));
-      } else {
+
+        // マップ表示をマーカ群を見渡せる範囲に拡張する
+        ztmp_zoom = target.map.getZoomLevel();
+        if (ztmp_zoom > DEFAULT_ZMAP_ZOOM_UPPER_LIMIT) {
+          ztmp_zoom = DEFAULT_ZMAP_ZOOM_UPPER_LIMIT;
+          target.map.zoomTo(ztmp_zoom);
+        } else if (ztmp_zoom < DEFAULT_ZMAP_ZOOM_LOWER_LIMIT) {
+          ztmp_zoom = DEFAULT_ZMAP_ZOOM_LOWER_LIMIT;
+          target.map.zoomTo(ztmp_zoom);
+        } else {
+        ztmp_zoom = ztmp_zoom - DEFAULT_ZMAP_ZOOM_CONTROL;
+        target.map.zoomTo(ztmp_zoom);
+        }
+      } else if (addresses_total == 1){
+      	// 取得地点にマーカをセット
+        putMarker(target, result.itemsAddr[0].pos, url);
+        // マーカが一つだけの時、マップ表示をマーカの位置に移動する
+        target.map.moveTo(result.itemsAddr[0].pos);
+      }	else {
         // 成功だけど結果0件
         console.log("no result retrieved.");
       }
@@ -168,16 +221,11 @@ function putMarker(target, latlng, url) {
   target.map.addGeometry(marker);
 }
 
-function extendRect(bounds, point) {
-  if (bounds) {
-    if (bounds.min > point) {
-      bounds.min = point;
-    }
-    if (bounds.max < point) {
-      bounds.max = point;
-    }
-  } else {
-    bounds = new ZntRect(point, point);
-  }
-  return bounds;
+function rect_Reset() {
+  // 前回の矩形の値をリセットする
+  x_max = 0;
+  x_min = 0;
+  y_max = 0;
+  y_min = 0;
+  ztmp_zoom = 0;
 }
