8f6ade98ea6e3b4b10e8cb805b382836f3a82184
suzuki-s <shinobusuzuki@di-system.co.jp>
2013-07-25 19:30:59 +0900

    bug fix

--- plugins/lgdis/db/seeds/projects.rb
+++ plugins/lgdis/db/seeds/projects.rb
@@ -6,7 +6,7 @@ ActiveRecord::Base.connection.execute(%{SELECT setval('enabled_modules_id_seq', 
 ActiveRecord::Base.connection.execute(%{DELETE FROM projects})
 ActiveRecord::Base.connection.execute(%{DELETE FROM projects_trackers})
 ActiveRecord::Base.connection.execute(%{SELECT setval('projects_id_seq', 1, FALSE)})
-ActiveRecord::Base.connection.execute(%{SELECT setval('projects_identifier_seq', 1, FALSE)})
+ActiveRecord::Base.connection.execute(%{SELECT setval('project_identifier_seq', 1, FALSE)})
 
 # 登録
 CSV.foreach("#{Rails.root}/plugins/lgdis/db/seeds/projects.csv", :headers => true) do |row|
